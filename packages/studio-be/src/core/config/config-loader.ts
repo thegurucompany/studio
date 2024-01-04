2102f5ac2d81e9b8dedbd2cd721818ee526e7b79
Gustavo Beauregard <gustavo@bwteconecta.com>
2024-01-04 05:19:31 -0600

    page title

--- packages/studio-be/src/core/config/config-loader.ts
+++ packages/studio-be/src/core/config/config-loader.ts
@@ -133,12 +133,12 @@ export class ConfigProvider {
   public async getBrandingConfig(appName: 'admin' | 'studio') {
     const defaultConfig = {
       admin: {
-        title: 'Botpress Admin Panel',
+        title: 'ChatO Studio Admin Panel',
         favicon: 'assets/admin/ui/public/favicon.ico',
         customCss: ''
       },
       studio: {
-        title: 'Botpress Studio',
+        title: 'ChatO Studio',
         favicon: 'assets/studio/ui/public/img/favicon.png',
         customCss: ''
       }
